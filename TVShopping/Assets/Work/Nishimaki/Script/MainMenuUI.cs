a099fa3243656c8b75fd5a732f1508536df48227
NishimakiKohei <deadblue0518@yahoo.co.jp>
2015-10-22 14:22:41 +0900

    テストもかねて自分のファイルを更新

---
+++ TVShopping/Assets/Work/Nishimaki/Script/MainMenuUI.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuUI : MonoBehaviour {
+
+    [SerializeField]
+    GUISkin Skin;
+
+    GUIStyle Style;
+    GUIStyleState State;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {	 
+	}
+
+    void OnGUI()
+    {
+
+        Style = new GUIStyle();
+        State = new GUIStyleState();
+
+        GUI.skin=Skin;
+
+        State.textColor = Color.white;
+
+        Style.fontSize = 30;
+        Style.normal = State;
+
+        if (GUI.Button(new Rect(0, 0, Screen.width/4, Screen.height/4), "カタログ"))
+        {
+            Application.LoadLevel("Catalog");
+        }
+
+        if (GUI.Button(new Rect(Screen.width / 2 + Screen.width / 4, 0, Screen.width / 4, Screen.height / 4), "オプション"))
+        {
+            Application.LoadLevel("Option");
+        }
+        
+    }
+}
