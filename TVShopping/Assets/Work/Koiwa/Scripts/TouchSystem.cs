101de0e34d418a337db78bcfb78549b005a822f3
rewish <spidertargal@yahoo.co.jp>
2015-11-02 03:53:10 +0900

    QTEアイコンの表示用スクリプトと、説明用mdメモ

---
+++ TVShopping/Assets/Work/Koiwa/Scripts/TouchSystem.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class TouchSystem : MonoBehaviour
+{
+    void Update()
+    {
+        isTouch();
+    }
+
+    // 画面のタッチ判定
+    bool isTouch()
+    {
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            Touch touch = Input.GetTouch(i);
+
+            // タッチした瞬間にtrueを返す(タッチを認識)
+            if (touch.phase == TouchPhase.Began)
+            {
+                Debug.Log("True!!");
+                return true;
+            }
+
+            // タッチを継続していてもtrueを返す
+            else if (touch.phase == TouchPhase.Stationary)
+            {
+                Debug.Log("Stationary!!");
+                return true;
+            }
+
+            // タッチを離した瞬間にfalseを返す
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                Debug.Log("False!!");
+                return false;
+            }
+        }
+        return false;
+    }
+}
---
+++ TVShopping/Assets/Work/Koiwa/TouchSystem.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class TouchSystem : MonoBehaviour {
+    
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+        isTouch();
+	}
+
+    // 反応が悪いのでマウスクリックでの操作は保留
+    //void isTouch()
+    //{
+    //    // クリックした瞬間に反応
+    //    if (Input.GetMouseButtonDown(0)) Debug.Log("左クリック");
+
+    //    // クリックを離した瞬間に反応
+    //    else if (Input.GetMouseButtonUp(0)) Debug.Log("左リリース");
+
+    //    else Debug.Log("");
+    //}
+
+    void isTouch()
+    {
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            Touch touch = Input.GetTouch(i);
+            Vector2 pos = touch.position;
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    Debug.Log("touch Began!");
+                    break;
+
+                case TouchPhase.Moved:
+                    Debug.Log("touch Moved!");
+                    break;
+
+                case TouchPhase.Stationary:
+                    Debug.Log("touch Stationary!");
+                    break;
+
+                case TouchPhase.Ended:
+                    Debug.Log("touch Ended!");
+                    break;
+
+                case TouchPhase.Canceled:
+                    Debug.Log("touch Canceled!");
+                    break;
+            };
+        }
+    }
+}
