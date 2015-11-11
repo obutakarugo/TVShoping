7c7e6cb811b13b6fd63ae7cf929eeefb7a875b39
rewish <spidertargal@yahoo.co.jp>
2015-11-12 01:09:15 +0900

    シーン遷移用のスクリプトを追加

---
+++ TVShopping/Assets/Work/Nishimaki/Menu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour
+{
+    TouchSystem _touch;
+    Scene_Manager _scene_manager;
+
+    // Use this for initialization
+    void Start()
+    {
+        _touch = GetComponent<TouchSystem>();
+        _scene_manager = GetComponent<Scene_Manager>();
+    }
+
+    [SerializeField]
+    Scene_Manager.SCENE_NAME scene;
+
+    void Update()
+    {
+        if (_touch.isTouch())
+        {
+            _scene_manager.NextScene(scene);
+        }
+    }
+}
